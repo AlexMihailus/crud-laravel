d40ce9b3b6ecf3ffb7eddf493c33c4188caa4d15
Alex Mihailus <mihailus.a.g@gmail.com>
2021-11-01 16:54:42 +0200

    add card for User

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,28 +3,32 @@
 @section('title', 'Users')
 
 @section('content')
+    <a class="btn btn-primary" role="button" href="{{ route('users.create') }}">Create user</a>
 
-<a class="btn btn-primary" role="button" href="{{ route('users.create') }}">Create user</a>
-<table class="table">
-    <thead>
+    <table class="table">
+        <thead>
         <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">Actions</th>
         </tr>
-    </thead>
-    <tbody>
+        </thead>
+        <tbody>
         @foreach($users as $user)
-        <tr>
-            <th scope="row">{{ $user->id }}</th>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>
-                <a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a>
-            </td>
-        </tr>
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <td>
+                    <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
+                </td>
+                <td>
+                    <a href="{{ route('users.show', $user) }}">{{ $user->email }}</a>
+                </td>
+                <td>
+                    <a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a>
+                </td>
+            </tr>
         @endforeach
-    </tbody>
-</table>
+        </tbody>
+    </table>
 @endsection
