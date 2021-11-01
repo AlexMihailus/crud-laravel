824e6768c53451a2d9a5f9ff36a919ca890d5ff3
Alex Mihailus <mihailus.a.g@gmail.com>
2021-11-01 22:17:13 +0200

    add flashes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,6 +17,7 @@
             -moz-user-select: none;
             user-select: none;
         }
+
         @media (min-width: 768px) {
             .bd-placeholder-img-lg {
                 font-size: 3.5rem;
@@ -24,14 +26,22 @@
     </style>
 
 </head>
+
 <body class="bg-light">
 
-<div class="container">
-    <h1>@yield('title')</h1>
-    <main>
-        @yield('content')
-    </main>
-</div>
+    <div class="container">
+        <h1>@yield('title')</h1>
+        <main>
+            @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+            @endisset
+            @if(session('danger'))
+            <div class="alert alert-danger">{{ session('danger') }}</div>
+            @endisset
+            @yield('content')
+        </main>
+    </div>
 
 </body>
+
 </html>
